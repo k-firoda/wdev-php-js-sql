4b5c0726e785f3dc2b8c0482d61830da5eedac0b
Rohit Jain <rohit@fullstacklearning.com>
2022-11-14 19:02:44 +0530

    changed book_form.php

---
+++ book_form.php
@@ -0,0 +1,18 @@
+<?php
+
+   $connection = mysqli_connect('localhost','root','','book_db');
+
+   if(isset($_POST['send'])){
+      extract($_POST);
+
+      $request = " insert into book_form (name, email, phone, address, location, guests, arrivals, leaving) values('$name','$email','$phone','$address','$location','$guests','$arrivals','$leaving') ";
+      $execute = mysqli_query($connection, $request);
+      if(!$execute)  echo mysqli_error($connection);
+
+      if(mysqli_insert_id($connection) > 0)
+         header('location:book.php'); 
+      else{
+         echo 'something went wrong please try again!';
+      }
+
+   }
